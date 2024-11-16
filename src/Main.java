17fda253b802f9be58a906c2b9fe5c120c356ef3
taliacoval <talia_coval27@milton.edu>
2024-11-15 19:07:56 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new Main();
     }
+
+public Main() {
+
+  Madlib Bob = new Madlib();
+    System.out.println("Today we have the pleasure of reading Talia's incredible madlib!");
+    System.out.println("Here it is...");
+    Bob.printStory();
 }
+}
+
